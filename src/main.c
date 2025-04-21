96e245123b909b3a5513d3716171e87e8cb4168d
Mahjabin Monika <zabin.mm@gmail.com>
2025-04-21 18:25:45 +0200

    update

--- src/main.c
+++ src/main.c
@@ -6,15 +6,42 @@
 /*   By: mmonika <mmonika@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 14:55:42 by mmonika           #+#    #+#             */
-/*   Updated: 2025/04/20 14:59:15 by mmonika          ###   ########.fr       */
+/*   Updated: 2025/04/21 18:17:08 by mmonika          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/miniRT.h"
 
+void	exit_hook(void *param)
+{
+	t_map	*map;
+
+	map = param;
+	if (mlx_is_key_down(map->mlx, MLX_KEY_ESCAPE))
+	{
+		// free(map->point);
+		mlx_terminate(map->mlx);
+		exit(0);
+	}
+}
+
 int	main(int argc, char *argv[])
 {
-	(void)argc;
-	(void)argv;
-	return (0);
+	t_map	*map;
+
+	if (argc != 2 || check_file(argv[1]) != 0)
+	{
+		printf("Usage: ./miniRT <filename.rt>\n");
+		return (FAIL);
+	}
+	map = map_initialization();
+	if (!map)
+		return (FAIL);
+	// dimension(argv[1], map);
+	// mem_alloc(map);
+	// parsing(argv[1], map);
+	// rendering(map);
+	mlx_loop_hook(map->mlx, &exit_hook, map);
+	mlx_loop(map->mlx);
+	return (SUCCESS);
 }
